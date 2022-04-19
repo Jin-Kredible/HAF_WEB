cf283ff85bfc165e2e54c9c870edc4b21d41da60
j <jinwkim90@gmail.com>
2022-04-20 07:39:46 +0900

    changed

--- src/pages/Presentation/index.js
+++ src/pages/Presentation/index.js
@@ -92,8 +92,7 @@ function Presentation() {
               px={{ xs: 6, lg: 12 }}
               mt={1}
             >
-              Free & Open Source Web UI Kit built over ReactJS &amp; MUI. Join over 1.6 million
-              developers around the world.
+              HAF 프로젝트 웹사이트입니다.
             </MKTypography>
           </Grid>
         </Container>
